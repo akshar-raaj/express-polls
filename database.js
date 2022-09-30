8a892f93cb9dc3ab0c93df909c64d73fc12b9bfc
Akshar Raaj <raaj.akshar@gmail.com>
2022-09-30 19:44:00 +0530

    Load database credentials from env variable

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
+const {DATABASE_CONNECTION_STRING, DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME} = require('./settings')
 
-const sequelize = new Sequelize('mysql://root:Up123pU!@localhost:3306/polls')
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {'host': DATABASE_HOST, 'dialect': 'mysql'})
 
 
 try {
