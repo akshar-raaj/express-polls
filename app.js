104086f5aef88c33f800d445c79e83f8bc797474
Akshar Raaj <raaj.akshar@gmail.com>
2022-09-30 13:10:19 +0530

    Added routes for detail, results and vote

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/polls/', (req, res) => {
   res.send(`Hello, world. You're at the polls index.`)
 })
 
+app.get('/polls/:pollId/', (req, res) => {
+  res.send(`You're looking at question ${req.params.pollId}`)
+})
+
+app.get('/polls/:pollId/results/', (req, res) => {
+  res.send(`You're looking at the results of question ${req.params.pollId}`)
+})
+
+app.get('/polls/:pollId/vote/', (req, res) => {
+  res.send(`You're voting on question ${req.params.pollId}`)
+})
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
