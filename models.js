bf077456a0daf160a1dd0e3502d7b21eaac4d795
Akshar Raaj <raaj.akshar@gmail.com>
2022-09-30 12:39:59 +0530

    Added an instance method on model Question. This tells us if a question was published recently.

--- models.js
+++ models.js
@@ -2,13 +2,26 @@ const { DataTypes, Model } = require('Sequelize');
 const db = require('./database')
 const connection = db.connection
 
-class Question extends Model {}
+class Question extends Model {
+    isPublishedRecently() {
+        // Tells if the question was published in the last 24 hours.
+        const currentTime = new Date(); // returns current time in UTC.
+        const allowedDifference = 86400000; // difference of 24 hours, in milliseconds.
+        let difference;
+        difference = currentTime - this.pubDate;
+        if (difference > allowedDifference) {
+            return false;
+        }
+        return true;
+    }
+}
 
 Question.init({
     questionText: {type: DataTypes.STRING, allowNull: false},
     pubDate: {type: DataTypes.DATE}
 },
     {sequelize:connection, tableName: 'questions'})
+
 
 class Choice extends Model {}
 Choice.init({
